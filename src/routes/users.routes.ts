fa774c21265b42d888a74f8342f2cf94413806db
Ken Ny <kennynguyen1631984@gmail.com>
2024-09-28 23:15:31 +0700

    Refactor users.routes.ts: Update user registration and login routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,35 @@
-import { Router } from "express";
-import { loginController, registerController } from "~/controllers/user.controllers";
+import { Router } from 'express'
+import { loginController, logoutController, registerController } from '~/controllers/user.controllers'
+import {
+  accessTokenValidation,
+  loginValidation,
+  refreshTokenValidation,
+  registerValidation
+} from '~/middlewares/users.middleware'
 
-const usersRouter = Router();
+const usersRouters = Router()
 
-usersRouter.post("/register", registerController);
+/* 
+Description: This route is used to register a new user
+Method: POST
+Body: { "username": "string", "email": "string", "password": "string", "confirmPassword": "string" ,"data_of_birth": ISO08601}
+ */
+usersRouters.post('/register', registerValidation, registerController)
 
-usersRouter.post("/login", loginController);
+/*
+Description: This route is used to login a user
+Method: POST
+Body: { "email": "string", "password": "string"}
+ */
+usersRouters.post('/login', loginValidation, loginController)
 
-export default usersRouter;
+/*
+Description: This route is used to logout
+Path: /logout
+Method: POST
+Headers: { Authorization : Bearer <accessToken> }
+Body: { refresh_token : string}
+*/
+usersRouters.post('/logout', accessTokenValidation, refreshTokenValidation, logoutController)
+
+export default usersRouters
