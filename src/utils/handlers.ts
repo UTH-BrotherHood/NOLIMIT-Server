886db246b9a92c633613686180cdcbb2b7a8161d
Ken Ny <kennynguyen1631984@gmail.com>
2024-09-30 16:24:32 +0700

    fix: wrapRequestHandler function to handle promise rejections

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -1,11 +1,20 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 
-export const wrapRequestHandler = <P>(fn: RequestHandler<P, any, any, any>) => {
-  return async (req: Request<P>, res: Response, next: NextFunction) => {
-    try {
-      fn(req, res, next)
-    } catch (error) {
-      next(error)
-    }
+// export const wrapRequestHandler = <P>(fn: RequestHandler<P, any, any, any>) => {
+//   return async (req: Request<P>, res: Response, next: NextFunction) => {
+//     try {
+//       fn(req, res, next)
+//     } catch (error) {
+//       next(error)
+//     }
+//   }
+// }
+
+export const wrapRequestHandler = (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      console.error('Error caught in wrapRequestHandler:', err)
+      next(err)
+    })
   }
 }
