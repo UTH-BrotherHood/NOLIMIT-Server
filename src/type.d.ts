28007de29ae1fe3bcbab510a6b84c9896641f908
lou1s vuong <lou1svuong.dev@gmail.com>
2024-10-06 19:54:52 +0700

    Refactor type declarations: Add user-related token payloads
    This commit adds new properties to the `Request` interface in the `express` module. The added properties include `user`, `decoded_authorization`, `decoded_refresh_token`, `decoded_email_verify_token`, and `decoded_forgot_password_token`. These properties are used to store token payloads related to user authentication and authorization.

---
+++ src/type.d.ts
@@ -0,0 +1,12 @@
+import { TokenPayload } from '~/models/requests/users.requests'
+import { UserDocument } from '~/models/schemas/user.schema'
+
+declare module 'express' {
+  interface Request {
+    user?: UserDocument
+    decoded_authorization?: TokenPayload
+    decoded_refresh_token?: TokenPayload
+    decoded_email_verify_token?: TokenPayload
+    decoded_forgot_password_token?: TokenPayload
+  }
+}
