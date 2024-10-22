6f91fc8b8b9f3ea859a67bf50465cfdb30608f6d
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-10-23 01:13:07 +0700

    Refactor: Update defaultErrorHandler in errors.middleware.ts to handle ErrorRequestHandler type

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -1,16 +1,22 @@
-import { Request, Response, NextFunction } from 'express'
-import { omit } from 'lodash'
-import HTTP_STATUS from '~/constants/httpStatus'
-import { ErrorWithStatus } from '~/utils/errors'
-export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { omit } from 'lodash';
+import HTTP_STATUS from '~/constants/httpStatus';
+import { ErrorWithStatus } from '~/utils/errors';
+
+// Định nghĩa rõ kiểu là ErrorRequestHandler
+export const defaultErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, 'status'))
+    res.status(err.status).json(omit(err, 'status'));
+    return;
   }
+
   Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
+    Object.defineProperty(err, key, { enumerable: true });
+  });
+
   res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
     message: err.message,
-    errorInfo: omit(err, 'stack')
-  })
-}
+    errorInfo: omit(err, 'stack'),
+  });
+  return;
+};
