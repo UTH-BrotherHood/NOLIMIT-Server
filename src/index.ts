12ecedd870259b2946e566895953954811dcb8c5
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-10-23 01:13:36 +0700

    Refactor: Add passport initialization and Google login route

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,21 @@ import { defaultErrorHandler } from '~/middlewares/errors.middleware'
 import rootRouterV1 from '~/routes'
 import databaseServices from '~/services/database.services'
 import cors from 'cors'
-
+import passport from "passport";
+import "~/utils/passport";
 const app = express()
 
+databaseServices.connect()
+
 app.use(cors())
+
+app.use(passport.initialize());
 
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1', rootRouterV1)
-
-databaseServices.connect()
 
 app.use(defaultErrorHandler)
 
