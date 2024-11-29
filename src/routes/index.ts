8c772ed98f919327be5128d7a699d9215c31fdc6
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-11-30 02:04:47 +0700

    feat: add tasks route and database service methods for task management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import conversationsRouter from '~/routes/conversations.routes'
 import groupsRouter from '~/routes/groups.routes'
 import participantsRouters from '~/routes/participants.routes'
 import messagesRouter from '~/routes/messages.routes'
+import tasksRouter from './tasks.routes'
 
 const rootRouterV1 = Router()
 rootRouterV1.use('/user', usersRouter)
@@ -11,5 +12,6 @@ rootRouterV1.use('/conversation', conversationsRouter)
 rootRouterV1.use('/group', groupsRouter)
 rootRouterV1.use('/participant', participantsRouters)
 rootRouterV1.use('/messages', messagesRouter)
+rootRouterV1.use('/task', tasksRouter)
 
 export default rootRouterV1
